5a86cb89577a254d84f03db498fdc330fce79700
da9dac <da9dac@gmail.com>
2024-03-28 20:58:05 +0900

    init

---
+++ kko-kko-noodles/src/main/java/org/ddeadbeat/kkokkonoodles/KkoKkoNoodlesApplication.java
@@ -0,0 +1,13 @@
+package org.ddeadbeat.kkokkonoodles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkoKkoNoodlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkoKkoNoodlesApplication.class, args);
+	}
+
+}
