0c14a5bde762d63542ce44bee7642ac1a7025124
da9dac <da9dac@gmail.com>
2024-03-29 17:27:56 +0900

    :sparkles: 회원 엔티티에 사용될 네임 VO 생성 및 테스트 #2

--- kko-kko-noodles/src/main/java/org/ddeadbeat/kkokkonoodles/member/domain/Member.java
+++ kko-kko-noodles/src/main/java/org/ddeadbeat/kkokkonoodles/member/domain/Member.java
@@ -1,5 +1,8 @@
 package org.ddeadbeat.kkokkonoodles.member.domain;
 
+import org.ddeadbeat.kkokkonoodles.member.domain.vo.Name;
+
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,4 +19,7 @@ public class Member {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Embedded
+	private Name name;
 }
